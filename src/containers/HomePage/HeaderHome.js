3ed71f345227d07f911e83e4cee482eb99551037
Tran-Bao-Anh <anh.tranbao.work@gmail.com>
2024-12-16 14:03:48 +0700

    change language with redux

--- src/containers/HomePage/HeaderHome.js
+++ src/containers/HomePage/HeaderHome.js
@@ -3,10 +3,18 @@ import { connect } from "react-redux";
 import "./HeaderHome.scss";
 import logo from "../../assets/images/logoBookingCare.svg";
 import { FormattedMessage } from "react-intl"; //giúp chuyển đỗi ngôn ngữ
+import { languages, LANGUAGES } from "../../utils";
+
+import { changeLanguageApp } from "../../store/actions";
 
 class HeaderHome extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
+
   render() {
-    console.log("check props: ", this.props);
+    let language = this.props.language;
+    console.log("check language: ", language);
 
     return (
       <>
@@ -63,15 +71,39 @@ class HeaderHome extends Component {
                 <i className="fas fa-question-circle"></i>
                 <FormattedMessage id="homeheader.support" />
               </div>
-              <div className="language-vi">VN</div>
-              <div className="language-en">EN</div>
+              <div
+                className={
+                  language === LANGUAGES.VI
+                    ? "language-vi active"
+                    : "language-vi"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                  VN
+                </span>
+              </div>
+              <div
+                className={
+                  language === LANGUAGES.EN
+                    ? "language-en active"
+                    : "language-en"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                  EN
+                </span>
+              </div>
             </div>
           </div>
         </div>
         <div className="home-header-banner">
           <div className="content-up">
-            <div className="title1"><FormattedMessage id="banner.title1"/></div>
-            <div className="title2"><FormattedMessage id="banner.title2"/></div>
+            <div className="title1">
+              <FormattedMessage id="banner.title1" />
+            </div>
+            <div className="title2">
+              <FormattedMessage id="banner.title2" />
+            </div>
             <div className="search">
               <i className="fas fa-search"></i>
               <input type="text" placeholder="Tìm chuyên khoa khám bệnh" />
@@ -83,37 +115,49 @@ class HeaderHome extends Component {
                 <div className="icon-child">
                   <i className="far fa-hospital"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child1"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child1" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-mobile-alt"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child2"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child2" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-procedures"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child3"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child3" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-flask"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child4"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child4" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-user-md"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child5"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child5" />
+                </div>
               </div>
               <div className="option-child">
                 <div className="icon-child">
                   <i className="fas fa-briefcase-medical"></i>
                 </div>
-                <div className="text-child"><FormattedMessage id="banner.child6"/></div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child6" />
+                </div>
               </div>
             </div>
           </div>
@@ -126,11 +170,14 @@ class HeaderHome extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language)=>dispatch(changeLanguageApp(language))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderHome);
